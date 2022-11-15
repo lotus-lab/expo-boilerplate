d2c9e4ea9777bf3182f918e2ed6a53f5ac5f5063
Humed-Muhammad <abc45171@gmail.com>
2022-11-15 10:25:46 +0300

    🚸 IMPROVE:  Dark mode

---
+++ src/utils/theme/default_dark/variables.ts
@@ -0,0 +1,14 @@
+const Colors = {
+  primary: "#6BCDFB",
+  text: "white",
+  inputBackground: "#545353",
+};
+
+const NavigationColors = {
+  primary: Colors.primary,
+};
+
+export default {
+  Colors,
+  NavigationColors,
+};
