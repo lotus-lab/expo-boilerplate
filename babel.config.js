5ac7ce3d032a809d421aa32de7923875322fec5b
Humed-Muhammad <abc45171@gmail.com>
2022-11-15 10:27:00 +0300

    🧹 CLEAN: 🔧 CONFIG:

--- babel.config.js
+++ babel.config.js
@@ -9,14 +9,6 @@ module.exports = function (api) {
           alias: {
             // This needs to be mirrored in tsconfig.json
             src: ["./src"],
-            // app: "./src/app",
-            // assets: "./src/assets",
-            // locales: "./src/locales",
-            // navigation: "./src/navigation",
-            // styles: "./src/styles",
-            // utils: "./src/utils",
-            // types: "./src/types",
-            // components: "./src/app/components",
           },
           root: ["./"],
           extensions: [".tsx", ".json", ".ts", ".js", ".jsx"],
