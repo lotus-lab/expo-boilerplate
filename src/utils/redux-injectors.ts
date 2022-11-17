6892ff261dcea6587541ebb426ea7d23ad909357
Humed-Muhammad <abc45171@gmail.com>
2022-11-17 10:33:25 +0300

    🚑 HOTFIX:

--- src/utils/redux-injectors.ts
+++ src/utils/redux-injectors.ts
@@ -7,16 +7,12 @@ import {
   InjectSagaParams,
   RootStateKeyType,
 } from "./types/injector-typings";
-import { configureAppStore } from "src/store/configureStore";
 
 /* Wrap redux-injectors with stricter types */
 
 export function useInjectReducer<Key extends RootStateKeyType>(
   params: InjectReducerParams<Key>
 ) {
-  const { store, reducer } = configureAppStore();
-  store.replaceReducer(reducer);
-  store.persistor.persist();
   return useReducer(params);
 }
 
